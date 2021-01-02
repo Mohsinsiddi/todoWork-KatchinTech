01e329fedb61642dda91a5e7a8aeaa9664965f4c
Md Mohsin Siddiqui <Mdmohsinsiddiqui5@gmail.com>
2021-01-02 21:01:29 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myapp';
+
+currentVal =''
+ 
+ getVal(val:string){ 
+   this.currentVal=val
+ }
+ 
 }
